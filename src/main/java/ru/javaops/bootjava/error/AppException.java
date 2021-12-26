35fe5300f0877852706a48dd5722bb21888bd630
-- <-->
2021-12-26 14:20:53 +0300

    5_01_account_controller

---
+++ src/main/java/ru/javaops/bootjava/error/AppException.java
@@ -0,0 +1,18 @@
+package ru.javaops.bootjava.error;
+
+import lombok.Getter;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+@Getter
+public class AppException extends ResponseStatusException {
+
+    private final ErrorAttributeOptions options;
+
+    public AppException(HttpStatus status, String msg, ErrorAttributeOptions options) {
+        super(status, msg);
+        this.options = options;
+
+    }
+}
--- src/main/java/ru/javaops/bootjava/util/ValidationUtil.java
+++ src/main/java/ru/javaops/bootjava/util/ValidationUtil.java
@@ -1,17 +1,20 @@
 package ru.javaops.bootjava.util;
 
+import ru.javaops.bootjava.error.IllegalRequestDataException;
 import ru.javaops.bootjava.model.BaseEntity;
 
 public class ValidationUtil {
 
     // проверяем что нам передали нового пользователя (без id)
     public static void checkNew(BaseEntity entity) {
-        if (!entity.isNew()) throw new IllegalArgumentException(entity + "должен быть новым (id = null)");
+        if (!entity.isNew()) throw new IllegalRequestDataException(
+                entity.getClass().getSimpleName() + " должен быть новым (id = null)");
     }
 
     //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
     public static void assureIdCosistent(BaseEntity entity, int id) {
         if (entity.isNew()) entity.setId(id);
-        else if (entity.getId() != id) throw new IllegalArgumentException(entity + "должен иметь id = " + id);
+        else if (entity.getId() != id) throw new IllegalRequestDataException(
+                entity.getClass().getSimpleName() + " должен иметь id = " + id);
     }
 }
